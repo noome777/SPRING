65aa0a3cf7392e1020bf2cdf36d1980b6eb8d893
AREUM\lee99 <lee990103@gmail.com>
2022-09-13 21:40:53 +0900

    spring 초기 작업 및 환경설정

--- springPrj04/src/main/java/com/kh/app04/member/dao/MemberDao.java
+++ springPrj04/src/main/java/com/kh/app04/member/dao/MemberDao.java
@@ -1,13 +1,11 @@
 package com.kh.app04.member.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
-
 import com.kh.app04.member.vo.MemberVo;
 
 public class MemberDao {
 	
 	public int join(SqlSessionTemplate sst, MemberVo vo) {
-		return sst.insert("", vo);
+		return ss.insert("", vo);
 	}
 
 }
