846e6d5e1bdc0df99be46aa37b86c2589c626a92
AREUM\lee99 <lee990103@gmail.com>
2022-09-15 13:40:54 +0900

    회원 정보 수정

--- springPrj05/src/main/java/com/kh/app05/member/service/MemberServiceImpl.java
+++ springPrj05/src/main/java/com/kh/app05/member/service/MemberServiceImpl.java
@@ -8,31 +8,35 @@ import com.kh.app05.member.dao.MemberDao;
 import com.kh.app05.member.vo.MemberVo;
 @Service
 public class MemberServiceImpl implements MemberService{
+		
+		@Autowired
+		private SqlSessionTemplate sst;
+		
+		@Autowired
+		private MemberDao dao;
 
-	@Autowired
-	private SqlSessionTemplate sst;
-	
-	@Autowired
-	private MemberDao dao;
-	
-	@Override
-	public int join(MemberVo vo) {
-		return dao.join(sst, vo);
+		@Override
+		public int join(MemberVo vo) {
+			return dao.join(sst , vo);
+		}
+
+		@Override
+		public MemberVo login(MemberVo vo) {
+			return dao.login(sst, vo);
+		}
+
+		@Override
+		public MemberVo edit(MemberVo vo) {
+			//업데이트
+			//업데이트 결과 확인하고, 다시 정보 조회
+			//다시 조회 해서 얻은 멤버를 리턴
+			return null;
+		}
+
+		@Override
+		public int quit(int memberNo) {
+			return 0;
+		}
+
 	}
 
-	@Override
-	public MemberVo login(MemberVo vo) {
-		return dao.login(sst, vo);
-	}
-
-	@Override
-	public MemberVo edit(MemberVo vo) {
-		return null;
-	}
-
-	@Override
-	public int quit(int memberNo) {
-		return 0;
-	}
-
-}
